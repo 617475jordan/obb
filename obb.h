7b0f78708bdfa50fd1e4a99c17818ddd7f0e33e8
nihongjie <nihongjie@51hitech.com>
2019-06-27 12:17:47 +0800

    nothing

--- obb.h
+++ obb.h
@@ -1,22 +1,51 @@
-#pragma once
+#ifndef _obb_H_
+#define _obb_H_
+
 #include <math.h>
 
 class obb {
 public:
-     obb(float x, float y, float width, float height, float rotation = 0.0f)
+    obb(float x, float y, float width, float height, float rotation = 0.0f)
         : _x(x), _y(y), _width(width), _height(height), _rotation(rotation) {
         resetVector();
     }
-    ~obb()
-    {
-      resetVector();
-    };
-    bool intersects(obb & other);
+
+    bool intersects(obb& other) {
+        float distanceVector[2] = {
+            other._x - _x,
+            other._y - _y
+        };
+
+        for (int i = 0; i < 2; ++i) {
+            if (getProjectionRadius(_vectors[i]) + other.getProjectionRadius(_vectors[i])
+                <= dot(distanceVector, _vectors[i])) {
+                return false;
+            }
+            if (getProjectionRadius(other._vectors[i]) + other.getProjectionRadius(other._vectors[i])
+                <= dot(distanceVector, other._vectors[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 private:
-    void resetVector();
-    float dot(float a[2], float b[2]);
-    float getProjectionRadius(float vector[2]) ;
+    void resetVector() {
+        _vectors[0][0] = cos(_rotation);
+        _vectors[0][1] = sin(_rotation);
+        _vectors[1][0] = -_vectors[0][1];
+        _vectors[1][1] = _vectors[0][0];
+    }
+
+    float dot(float a[2], float b[2]) {
+        return abs(a[0] * b[0] + a[1] * b[1]);
+    }
+
+    float getProjectionRadius(float vector[2]) {
+        return (_width * dot(_vectors[0], vector) / 2
+            + _height * dot(_vectors[1], vector) / 2);
+    }
 
     float _x;
     float _y;
@@ -25,3 +54,5 @@ private:
     float _rotation;
     float _vectors[2][2];
 };
+
+#endif // _obb_H_
--- source.cpp
+++ source.cpp
@@ -1,37 +1,41 @@
-#include  <iostream>
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
 #include "obb.h"
 using namespace std;
 
 int main()
 {
-  float _x=10;
-  float _y=0;
-  float _width=10;
-  float _height=10;
-  float _rotation=0;
+  double _x=10;
+  double _y=0;
+  double _width=10;
+  double _height=10;
+  double _rotation=0;
   obb *m_obb=new obb( _x,  _y,  _width,  _height, _rotation);
   //obb n_obb(_x+2*_width, _y+2*_height, _width+1, _height+1, _rotation);
   std::cout << "Num   x   y   width   height   flag " << std::endl;
   for (unsigned int i = 0; i < 1000; i++)
   {
-	  float m_x = (rand() % 10001) / 1000.0;
-	  float m_y= (rand() % 10001) / 1000.0;
-	  float m_width= (rand() % 10001) / 1000.0;
-	  float m_height = (rand() % 10001) / 1000.0;
-	  float m_rotation= (rand() % 10001) / 1000.0;
+	  double m_x = (rand() % 10001) / 1000.0;
+	  double m_y= (rand() % 10001) / 1000.0;
+	  double m_width= (rand() % 10001) / 1000.0;
+	  double m_height = (rand() % 10001) / 1000.0;
+	  double m_rotation= (rand() % 10001) / 1000.0;
 
 
-	  obb n_obb(m_x, m_y, m_width, m_height, m_rotation);
+	  obb *n_obb=new obb(m_x, m_y, m_width, m_height, m_rotation);
 	  bool flag = false;
-	  flag = m_obb->intersects(n_obb);
-	  
+	  flag = m_obb->intersects(*n_obb);
+    n_obb=NULL;
+    delete[]n_obb;
 	  //std::cout <<"Num:"<<i+1<<"x:"<<m_x<<"y:"<<m_y<<"width"<<m_width<<"height:"<<m_height<<"flag:"<< flag << std::endl;
 	  std::cout << i + 1 << "  " << m_x << "  " << m_y << "  " << m_width << "  " << m_height << "  " << flag << std::endl;
 
   }
   m_obb = NULL;
   delete[] m_obb;
+#if _Win32
   system("pause");
-
+#endif
   return 0;
 }
